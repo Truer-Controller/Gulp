66211d05f5098501da32dd1d21e5984a8e9aec0a
Rafael Augusto <rafajunior2907@gmail.com>
2023-12-18 19:02:14 -0300

    copiando arquivos

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp')
+const { series, parallel} = require('gulp')
+
+const antes1 = cb => {
+    console.log('Tarefa antes 1')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa antes 2')
+    return cb()
+}
+
+function copiar(cb) {
+    //gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) Copia apenas os arquivos que estão sendo enviados por array
+    gulp.src('pastaA/**/*.txt')
+    .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa antes fim')
+    return cb()
+}   
+
+module.exports.default = series(
+    parallel(antes1,antes2),
+    copiar,
+    fim,)
